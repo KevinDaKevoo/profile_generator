7f007612ca3774c80c679a205b182a38a46603b7
Kevin Yu <Kevin.yu.kfy@gmail.com>
2021-06-01 16:01:48 +0000

    Updated errors in survery.js

--- survey.js
+++ survey.js
@@ -6,26 +6,25 @@ const rl = readline.createInterface({
 });
 
 rl.question(`What's your name? Nicknames are also acceptable 😄: `, (answer) => {
-
   console.log(`Your name is: ${answer}`);
   
   rl.question(`What's an activity you like doing? `, (answer) => {
     console.log(`Liked activity: ${answer}`);
 
     rl.question(`What do you listen to while doing that? `, (answer) => {
-        console.log(`I listen to: ${answer}`);
+      console.log(`I listen to: ${answer}`);
 
       rl.question(`Which meal is your favourite? (e.g: dinner, brunch, etc.) `, (answer) => {
-            console.log(`Favourite meal: ${answer}`);
+        console.log(`Favourite meal: ${answer}`);
 
         rl.question(`What's your favourite thing to eat for the meal? `, (answer) => {
-                console.log(`Favourite thing to eat: ${answer}`);
+          console.log(`Favourite thing to eat: ${answer}`);
 
           rl.question(`Which sport is your absolute favourite? `, (answer) => {
-                    console.log(`Favourite sport: ${answer}`);
+            console.log(`Favourite sport: ${answer}`);
 
             rl.question(`what is your superpower? (In a few words, tell us what you are amazing at!) `, (answer) => {
-                        console.log(`Superpower: ${answer}`);
+              console.log(`Superpower: ${answer}`);
               rl.close();
             });
           });
